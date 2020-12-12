eb5f5735714f230face11bd2def0a6b5301bb8fe
aman <amanking938@gmail.com>
2020-12-12 16:42:52 +0530

    inti_2

---
+++ app/src/main/java/com/example/myapplication_password_modulation/sixth_screen.java
@@ -0,0 +1,14 @@
+package com.example.myapplication_password_modulation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sixth_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sixth_screen);
+    }
+}
